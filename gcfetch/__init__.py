1d3c8d59e352ec441b091f9a5447465935c68d64
David Miller <David>
2012-10-24 20:28:08 +0100

    Use graphs to store the sitemap Use Networkx's matplotlib integration to visualise

--- gcfetch/__init__.py
+++ gcfetch/__init__.py
@@ -11,23 +11,48 @@ import urlparse
 
 import argparse
 from lxml import html
+import matplotlib.pyplot as plt
+import networkx
+import pylab
 import requests
 
 from _version import __version__
 
-# C&P from another project
+DONE = """
+*******************************************************
+Dear valued user,
+
+Your sitemap is ready to view at {0}.
+
+Please use your favourite image viewer to take a look.
+"""
+
+PROTORE = re.compile(r'https?:{0,1}/{1,2}')
+
+# originally C&P from another project
 def protocolise(url):
     """
     Given a URL, check to see if there is an assocaited protocol.
 
     If not, set the protocol to HTTP and return the protocolised URL
     """
-    # Use the regex to match http//localhost/something
-    protore = re.compile(r'https?:{0,1}/{1,2}')
     parsed = urlparse.urlparse(url)
-    if not parsed.scheme and not protore.search(url):
+    if not parsed.scheme and not PROTORE.search(url):
         url = 'http://{0}'.format(url)
     return url
+
+def deprotocolise(url):
+    """
+    If URL has a protocol, strip it.
+
+    Arguments:
+    - `url`: str
+
+    Return: str
+    Exceptions: None
+    """
+    return PROTORE.sub('', url)
+
 
 def indomain(url, domain):
     """
@@ -89,7 +114,7 @@ def getlinks_to(markup, domain):
 
     return links
 
-def fetch_website(sitemap, base, url):
+def fetch_website(sitemap, seen, base, url):
     """
     Recursive function to fill SITEMAP with the documents that
     make up BASE, looking at URL at the first pass.
@@ -103,6 +128,7 @@ def fetch_website(sitemap, base, url):
     Return: None
     Exceptions: None
     """
+    print url
     # Use requests because lxml's default fetch url code doesn't
     # respect 301 redirects
     req = requests.get(url)
@@ -110,15 +136,42 @@ def fetch_website(sitemap, base, url):
     root = html.document_fromstring(req.content)
     root.make_links_absolute(base)
 
-    sitemap[url]['statics'] = getstatic(root)
-    sitemap[url]['links'] = getlinks_to(root, base)
+    seen.add(url)
 
-    for link in sitemap[url]['links']:
-        if link not in sitemap:
-            fetch_website(sitemap, base, link)
+    statics = getstatic(root)
+    links = getlinks_to(root, base)
+
+    sitemap.add_node(url, statics=statics, links=links)
+
+    for link in links:
+        sitemap.add_edge(url, link)
+
+        if link not in seen:
+            fetch_website(sitemap, seen, base, link)
 
     return
 
+def output(sitemap, domain):
+    """
+    Produce our representation of the graph SITEMAP
+
+    Arguments:
+    - `graph`: networkx.DiGraph
+
+    Return: None
+    Exceptions: None
+    """
+    labels = dict((n, n +"\n" + ("-" * len(n)) + "\n" + "\n".join(d['statics'])) for n,d in sitemap.nodes(data=True))
+    networkx.draw(sitemap, labels=labels)
+    # Adjust the size up to the point at which it's useful
+    F = pylab.gcf()
+    DPI = F.get_dpi()
+    DEF = F.get_size_inches()
+    F.set_size_inches(DEF[0] * 5, DEF[1] * 5)
+
+    filename = '{0}.sitemap.png'.format(domain)
+    plt.savefig(filename)
+    return filename
 
 def main(args):
     """
@@ -134,10 +187,13 @@ def main(args):
     # Allow loose commandline args
     base = protocolise(args.domain)
 
-    sitemap = collections.defaultdict(lambda: collections.defaultdict(list))
-    fetch_website(sitemap, base, base)
+    seen = set()
+    sitemap = networkx.DiGraph()
+    fetch_website(sitemap, seen, base, base)
 
-    print json.dumps(sitemap, indent=2)
+    outfile = output(sitemap, deprotocolise(base))
+
+    print DONE.format(outfile)
 
     return 0
 
